e3ee2a82b7acff7d0c2ba27df146b7460eb53582
Johan Espino <johancarp9@gmail.com>
2020-02-19 12:20:40 -0500

    [UPDATE] create my first component

---
+++ src/MyComponet.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+ 
+class MyComponent extends Component {
+ render(){
+   return (<div>This is my first component</div>)
+ }
+}
+ 
+export default MyComponent
