08ec9ff6fe49aa1f654a8fd3f962d7eb7ba7891b
Johan Espino <johancarp9@gmail.com>
2020-02-19 15:44:57 -0500

    [UPDATE] Add useState and buttons to change counter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MyComponent from './MyComponet'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ count, setCount ] = useState(0)
+
+  const addMount = () => {
+    setCount(count+1)
+  } 
+
+  const removeMount = () => {
+    setCount(count-1)
+  } 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <MyComponent name="Johan"/>
+        <button onClick={addMount}>
+          +
+        </button>
+        <button onClick={removeMount}>
+          -
+        </button>
+        <h1>{count}</h1>
       </header>
     </div>
   );
