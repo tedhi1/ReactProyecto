51aecb8557c83c686030cbf06ec8522b748b6afd
Johan Espino <johancarp9@gmail.com>
2020-02-19 15:01:52 -0500

    [UPDATE] Change class component to function component

--- src/MyComponet.js
+++ src/MyComponet.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
  
-class MyComponent extends Component {
+// Function Component
+const MyComponent = (props) => {
+  return(<div>My name is {props.name}</div>)
+}
+
+// Function Component
+/* function MyComponent (props) {
+  return(<div>My name is {props.name}</div>)
+} */
+
+// Class Component
+/* class MyComponent extends Component {
  render(){
  return (<div>My name is {this.props.name}</div>)
  }
-}
+} */
  
 export default MyComponent
